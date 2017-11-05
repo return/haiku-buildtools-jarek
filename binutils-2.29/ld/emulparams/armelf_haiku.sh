2c3d0d18ce0279a251d4fd5065b876e18e166e66
Jaroslaw Pelczar <jarek@jpelczar.com>
2017-11-05 18:53:47 +0100

    binutils-2.29: generate EABI unwind sections for ARM
    This is for armelf haiku target
    
    Signed-off-by: Jaroslaw Pelczar <jarek@jpelczar.com>

--- binutils-2.29/ld/emulparams/armelf_haiku.sh
+++ binutils-2.29/ld/emulparams/armelf_haiku.sh
@@ -25,3 +25,9 @@ NO_SMALL_DATA=yes
 
 . ${srcdir}/emulparams/elf_haiku.sh
 
+# Use the ARM ABI-compliant exception-handling sections.
+OTHER_READONLY_SECTIONS="
+  .ARM.extab ${RELOCATING-0} : { *(.ARM.extab${RELOCATING+* .gnu.linkonce.armextab.*}) }
+  ${RELOCATING+ PROVIDE_HIDDEN (__exidx_start = .); }
+  .ARM.exidx ${RELOCATING-0} : { *(.ARM.exidx${RELOCATING+* .gnu.linkonce.armexidx.*}) }
+  ${RELOCATING+ PROVIDE_HIDDEN (__exidx_end = .); }"
